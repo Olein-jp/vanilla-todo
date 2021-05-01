24d41d823330bea12c99170e6302389bf190d7ec
Koji Kuno <koji.kuno@gmail.com>
2021-05-01 16:46:16 +0900

    TODOタイトルが入力され追加ボタンが押されたら input 内を次のタスク入力のために初期化する

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./styles.css";
 const onClickAdd = () => {
   // 3. input に入力された TODO タイトルを取得する
   const inputTaskTitle = document.getElementById("input-task-title").value;
+  // 4. TODOタイトルが入力され追加ボタンが押されたら input 内を次のタスク入力のために初期化する
+  document.getElementById("input-task-title").value = "";
 };
 
 // 2. 追加ボタンがクリックされたら発火するイベントを作成
