395078f543f8c1d2cb6fd4e6bb96b3720cb0e329
Koji Kuno <koji.kuno@gmail.com>
2021-05-01 16:36:07 +0900

    タスクを追加ボタンに `#add-button` を付与して、ボタンを押した際の処理の雛形を作成する

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+// 2. クリックイベントを作成
+const onClickAdd = () => {
+  alert("追加ボタンをクリック");
+};
+
+// 2. 追加ボタンがクリックされたら発火するイベントを作成
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
